fee59070a1d77b1371427c4f22bf591addf9c00f
Pankaj Aswal <89585289+pankajaswal888@users.noreply.github.com>
2025-08-02 20:47:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+  default     = "myapp"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 2
+}
